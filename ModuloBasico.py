9feebe17042b330b89e06f8ad37450629dbd9f43
sergioruizperez14 <sergio.ruiz.perez.alu@iesjulioverne.es>
2026-03-10 10:39:06 +0100

    Subir primeros

---
+++ ModuloBasico.py
@@ -0,0 +1,32 @@
+# Example file showing a circle moving on screen
+import pygame
+import time
+import Mazinger
+
+# Añadir aquí las importaciones del armamento.
+import Planeador
+import PuñosFuera
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((700, 800))
+running = True
+
+maz=Mazinger.Mazinger(screen)
+
+# Incorporar aquí el armamento con pares de líneas como éstos.
+planeador= Planeador.Planeador()
+maz.incorpora(planeador)
+puños= PuñosFuera.PuñosFuera()
+maz.incorpora(puños)
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    maz.arm(screen)
+    time.sleep(0.5)
+    maz.desactiva(screen)
+    time.sleep(0.5)
+    maz.sigArm()
